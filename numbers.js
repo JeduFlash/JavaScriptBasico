5c650f43040f072c12dfc4d39dab6684769b51f2
JeduFlash <jhonatansix@gmail.com>
2019-10-07 21:16:46 -0500

    hola mundo

---
+++ numbers.js
@@ -0,0 +1,4 @@
+
+const  ejemplo = 123456789 ;
+
+console.log(ejemplo);
---
+++ variables.js
@@ -0,0 +1,5 @@
+//const example  ='some string';
+const someString = 'this is a string';
+
+//console.log(someString)
+console.log(example);
